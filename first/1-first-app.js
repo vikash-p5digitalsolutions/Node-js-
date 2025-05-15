db1d65e996609ebc866ab3d0fd798e31eeb09223
Vikash Mishra <vikash.p5digitalsolutions@gmail.com>
2025-05-15 18:27:27 +0530

    Add files via upload

--- first/1-first-app.js
+++ first/1-first-app.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
+const requestListener = (request, response) => {
 	response.write('Hello from different World\n');
 	response.end();
-});
+	// response.end('Hello from different World\n');
+}
 
-server.listen(9000, () => {
+const server = http.createServer(requestListener);
+
+const confirmationCallback = () => {
 	console.log('Server is running...');
-});
+}
+
+server.listen(9000, confirmationCallback);
